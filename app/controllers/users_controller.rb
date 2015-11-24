2298a6a5e3e18137a879a1c3fa5cd7ca25e817b0
Daphne <dg1181@nyu.edu>
2015-11-24 00:15:57 -0800

    Current user will redirect to links page from root route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   def new
-    @disable_nav = true
-    current_user
+    if current_user
+      redirect_to links_path
+    else
+      @disable_nav = true
+      current_user
+    end
   end
 end
