d5148c2d1f98d2a34eb4ba2bd2ce899b94b4ffaf
Daphne <dg1181@nyu.edu>
2015-11-24 22:05:43 -0800

    Attempt to update nickname column in database when users login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,9 @@ class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    if user.nickname.nil?
+      user.nickname = auth["info"]["nickname"]
+    end
     session[:user_id] = user.id
     redirect_to links_path
   end
