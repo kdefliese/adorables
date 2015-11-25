f160cff68bc5cb697116981946c5c58d6e6a2566
Daphne <dg1181@nyu.edu>
2015-11-24 16:35:15 -0800

    added after_destroy to delete associated votes and comments for a link

--- app/models/link.rb
+++ app/models/link.rb
@@ -5,6 +5,11 @@ class Link < ActiveRecord::Base
 
   validates :url, format: {with: /\.(png|jpg|gif|jpeg)\Z/i}
 
+  after_destroy do
+    Vote.destroy_all(link_id: self.id)
+    Comment.destroy_all(link_id: self.id)
+  end
+
   def total_votes
     self.votes.inject(0) { |sum, vote| sum + vote.value }
   end
