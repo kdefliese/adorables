ce8798080961f7b1b5cd98ea76659eb299e96601
Daphne <dg1181@nyu.edu>
2015-11-24 22:07:52 -0800

    Forgot to save it

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     if user.nickname.nil?
       user.nickname = auth["info"]["nickname"]
+      user.save
     end
     session[:user_id] = user.id
     redirect_to links_path
