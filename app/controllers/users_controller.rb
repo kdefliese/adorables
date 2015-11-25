f366e55207d0b283caabb86a1ccaae960ecb0160
Daphne <dg1181@nyu.edu>
2015-11-24 18:44:17 -0800

    Fixed some unnecessary code in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,7 @@ class UsersController < ApplicationController
   def new
     if current_user
       redirect_to links_path
-    else
-      @disable_nav = true
-      current_user
     end
+    @disable_nav = true
   end
 end
