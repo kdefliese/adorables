a4e290e8808af78913334600b4bd16edfa884bb5
Katherine Defliese <kdefliese@users.noreply.github.com>
2016-01-02 22:54:36 -0800

    added more logic to profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
 
   def profile
     @user = current_user
+    @links = current_user.links
+    @comments = current_user.comments
+    @votes = current_user.votes
   end
 
 end
